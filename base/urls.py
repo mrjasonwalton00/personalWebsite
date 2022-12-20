aa9b491c4af3fcb88c2d4b275f64fae5091d2daa
mrjasonwalton00 <jpwalton00@gmail.com>
2022-12-20 16:30:48 -0500

    css stuff

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.jason, name='jason'),
+    path('', views.home, name='home'),
     path('page1/', views.page1, name='page1'),
     path('page2/', views.page2, name='page2'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def jason(request):
-    return render(request, 'jason.html')
+def home(request):
+    return render(request, 'base/home.html')
 
 def page1(request):
-    return render(request, 'page1.html')
+    return render(request, 'base/page1.html')
 
 def page2(request):
-    return render(request, 'page2.html')
+    return render(request, 'base/page2.html')
